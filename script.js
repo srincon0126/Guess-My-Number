601b387cb1917fe97772be3d8f97b34048159d1a
srincon0126 <108340030+srincon0126@users.noreply.github.com>
2024-01-27 23:18:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.message').textContent);
 
-// document.querySelector('.message').textContent = 'Correct number!';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-// secret number
+document.querySelector('.message').textContent = 'Correct number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+secret number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
